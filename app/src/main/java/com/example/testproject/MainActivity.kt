d23b7c9c0feb4fc7b48796ce0eddabb9d6584741
ks <k.sampers@fontys.nl>
2023-10-17 16:53:04 +0200

    GPS location

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -4,9 +4,14 @@ import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.Manifest.permission.LOCATION_HARDWARE
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -44,7 +49,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.testproject.ui.theme.TestProjectTheme
+import java.io.Console
 
 class MainActivity : ComponentActivity() {
 
@@ -65,6 +72,35 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private var locationByGps: Location? = null
+    private var locationByNetwork: Location? = null
+    private var currentLocation: Location? = null
+    lateinit var locationManager: LocationManager
+    private var longitude: Double = 0.0;
+    private var latitude: Double = 0.0;
+
+
+
+    val gpsLocationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            locationByGps= location
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+    //------------------------------------------------------//
+    val networkLocationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            locationByNetwork= location
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,6 +112,59 @@ class MainActivity : ComponentActivity() {
                 this,
                 ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
+
+
+                locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                val hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                val hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+
+
+                if (hasGps) {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        5000,
+                        0F,
+                        gpsLocationListener
+                    )
+                }
+                if (hasNetwork) {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        5000,
+                        0F,
+                        networkLocationListener
+                    )
+                }
+
+                val lastKnownLocationByGps =
+                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                lastKnownLocationByGps?.let {
+                    locationByGps = lastKnownLocationByGps
+                }
+
+                val lastKnownLocationByNetwork =
+                    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                lastKnownLocationByNetwork?.let {
+                    locationByNetwork = lastKnownLocationByNetwork
+                }
+
+                if (locationByGps != null && locationByNetwork != null) {
+                    if (locationByGps!!.accuracy > locationByNetwork!!.accuracy) {
+                        currentLocation = locationByGps
+                        latitude = currentLocation!!.latitude
+                        longitude = currentLocation!!.longitude
+                        // use latitude and longitude as per your need
+                    } else {
+                        currentLocation = locationByNetwork
+                        latitude = currentLocation!!.latitude
+                        longitude = currentLocation!!.longitude
+                        // use latitude and longitude as per your need
+                    }
+                }
+
+                Log.d("Location", "Longitude: $longitude, Latitude: $latitude")
+
                 setContent {
                     Main()
                 }
@@ -90,7 +179,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun Main(){
     TestProjectTheme {
